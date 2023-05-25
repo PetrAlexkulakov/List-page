d2d9c08f18e2d9ed89f395f7e3052606b6336f8f
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-05-25 20:38:20 +0400

    refactor: delete useless

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,6 +1,5 @@
 import AppRouter from '../AppRouter';
 import './App.css';
-import React from 'react';
 
 const App = () => {
   return (
--- src/components/AppRouter/index.tsx
+++ src/components/AppRouter/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import List from "../../pages/List";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
