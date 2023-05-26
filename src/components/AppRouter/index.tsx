c65b68d93137acb14a0711a47b692ebf46f13d74
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-05-26 19:57:52 +0400

    fix: fix routes

--- src/components/AppRouter/index.tsx
+++ src/components/AppRouter/index.tsx
@@ -1,12 +1,12 @@
 import List from "../../pages/List";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const AppRouter = () => {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/navigator" />}/>
-          <Route path="/navigator" element={<List />} />
+          <Route path="/" element={<List />} />
+          <Route path="/navigator" element={<>Nothing here yet</>} />
         </Routes>
       </BrowserRouter>
     );
